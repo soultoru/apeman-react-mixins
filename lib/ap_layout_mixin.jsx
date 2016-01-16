06754ac48dbd04ce92b49d53b06d795294061bf9
Taka Okunishi <okunishitaka.com@gmail.com>
2016-01-16 23:18:49 +0900

    Version incremented to 1.5.3

--- lib/ap_layout_mixin.jsx
+++ lib/ap_layout_mixin.jsx
@@ -8,7 +8,7 @@
 import React, {PropTypes as types} from 'react';
 
 const HANDLER_NOT_IMPLEMENTED_WARNING = '[ApLayoutMixin] Should implement doLayout method.';
-const LAYOUT_INTERVAL = 100;
+const LAYOUT_INTERVAL = 40;
 
 /** @lends ApResizeMixin */
 let ApResizeMixin = {
@@ -16,13 +16,13 @@ let ApResizeMixin = {
     //--------------------
     // Custom
     //--------------------
-    _layoutIfNeeded(){
+    _layoutIfNeeded(immidiate){
         let s = this;
 
         clearTimeout(s._layoutTimer);
         s._layoutTimer = setTimeout(()=> {
             s.doLayout();
-        }, LAYOUT_INTERVAL);
+        }, immidiate ? 0 : LAYOUT_INTERVAL);
     },
 
     /**
@@ -48,10 +48,11 @@ let ApResizeMixin = {
         let s = this;
         if (!s.doLayout) {
             console.warn(HANDLER_NOT_IMPLEMENTED_WARNING);
+            return;
         }
         window.addEventListener('resize', s._layoutIfNeeded);
-
-        setTimeout(s._layoutIfNeeded, 10);
+        s._layoutIfNeeded(true);
+        s._layoutIfNeeded();
     },
 
     componentWillReceiveProps(nextProps) {
